d71d38f7dfd541f4dc276e6c2d3973cf877a33e9
Aaron Bertell <aaronbertell@Aarons-MacBook-Pro.local>
2022-07-31 20:02:09 -0700

    updated

--- rdt_layer.py
+++ rdt_layer.py
@@ -140,13 +140,19 @@ class RDTLayer(object):
         # The data is just part of the entire string that you are trying to send.
         # The seqNum is the sequence number for the segment (in character number, not bytes)
 
+        # calculate total chars left to send
         charsLeftToSend = len(self.dataToSend) - self.charCount
+        # calculate packets based on the chars left to send and the data length constant
         totalPacketsLeft = math.ceil(charsLeftToSend / self.DATA_LENGTH)
+        # segments to send is the rounded down flow control window size divided by the payout size
         segmentsToSend = math.floor(self.FLOW_CONTROL_WIN_SIZE / self.DATA_LENGTH)
-
+        # if however, the total packets left is less than the current segments to send, then set segements to send
+        # to the total packets left. So basically, when there is less than 3 packets to send, total packets left will
+        # be either 1 or 2, and so that will be what segments to send is set to.
         if totalPacketsLeft < segmentsToSend:
             segmentsToSend = totalPacketsLeft
 
+        # pipline segments depending on the flow control window size and the data length constants
         for p in range(segmentsToSend):
             segmentSend = Segment()
             data = ''
@@ -188,19 +194,26 @@ class RDTLayer(object):
         # This call returns a list of incoming segments (see Segment class)...
         # receive() returns a list of all the sent packets
 
+        # get packets from receive channel
         listIncomingSegments = self.receiveChannel.receive()
-        #listIncomingSegments += self.receivingListHistory
-        #self.receivingListHistory.clear()
+        # add segments just received to the segments that have been delayed in processing
+        listIncomingSegments += self.receivingListHistory
+        # clear the delayed segments array
+        self.receivingListHistory.clear()
 
-        self.bubbleSort(listIncomingSegments)
+        # sort the current listIncomingSegments to be viewed
+        self.segmentBubbleSort(listIncomingSegments)
+        # loop through all the segments in the list incoming segments that have now been ordered
         for seg in listIncomingSegments:
 
             seg.printToConsole()
-            # if the payload of the segment is not empty then it is a data segment else it is an ACK
+            # if the sequence number matches the ack number we are looking for then process it, if not then it
+            # is out of order, and we just put that segment into the receiving list history array to look at
+            # on the next processing, and skip this iteration.
             if seg.seqnum > -1:
-                #if seg.seqnum != self.ackNum:
-                #    self.receivingListHistory.append(seg)
-                #    continue
+                if seg.seqnum != self.ackNum:
+                    self.receivingListHistory.append(seg)
+                    continue
 
                 # #################################################################################################### #
                 # What segments have been received?
@@ -216,9 +229,7 @@ class RDTLayer(object):
                 # Somewhere in here you will be setting the contents of the ack segments to send.
                 # The goal is to employ cumulative ack, just like TCP does...
                 print("Value we should be comparing it to")
-                print(self.ackNum)
-                print(seg.seqnum)
-                self.ackNum = seg.seqnum + 1
+                self.ackNum += len(seg.payload)
 
                 # #################################################################################################### #
                 # Display response segment
@@ -228,17 +239,14 @@ class RDTLayer(object):
                 # Use the unreliable sendChannel to send the ack packet
                 self.sendChannel.send(segmentAck)
 
-    def bubbleSort(self, arr):
+    def segmentBubbleSort(self, arr):
         n = len(arr)
-        # optimize code, so if the array is already sorted, it doesn't need
-        # to go through the entire process
-        swapped = False
+        swap = False
         # Traverse through all array elements
         for i in range(n - 1):
             for j in range(0, n - i - 1):
                 if arr[j].seqnum > arr[j + 1].seqnum:
-                    swapped = True
+                    swap = True
                     arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-            if not swapped:
+            if not swap:
                 return
